6b0af9b2b38d9030612f89201a1006506014e2aa
RLH-2110 <151957057+RLH-2110@users.noreply.github.com>
2025-09-04 14:50:27 +0200

    Create fileStrings.c

---
+++ string/fileStrings.c
@@ -0,0 +1,26 @@
+#include <string.h>
+
+/* concats base_path and filename, adds a '/' if nessesary. memory is set to NULL on error. frees memory if not NULL*/
+void constuct_filename(char* base_path, char* filename, char** memory){
+	char* writePtr;
+
+	if (!base_path || !filename) /* base_path and filename cant be NULL*/
+		return;
+
+	if (*memory != NULL)
+		free(*memory);
+
+	*memory = malloc(strlen(base_path)+1+strlen(filename)+1); /* enough space for the new filename */
+
+	strcpy(*memory,base_path); /* copy base path*/
+
+	/* add / */
+
+	if (strlen(*memory) > 0 && (*memory)[strlen(*memory)]-1 != '/'){ /* if there is no trailing / */
+		writePtr = *memory + strlen(*memory);
+		*writePtr = '/'; writePtr++;
+	}
+
+	/* add filename*/
+	strcpy(writePtr,filename);
+}
